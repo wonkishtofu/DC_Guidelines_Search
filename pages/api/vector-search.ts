7425f58934f28202f8b019ce04630c4a69479cb0
Tan Xin Yi <08tanx@gmail.com>
2023-07-04 12:12:28 +0800

    first commit

---
+++ pages/api/vector-search.ts
@@ -0,0 +1,179 @@
+import type { NextRequest } from 'next/server'
+import { createClient } from '@supabase/supabase-js'
+import { codeBlock, oneLine } from 'common-tags'
+import GPT3Tokenizer from 'gpt3-tokenizer'
+import {
+  Configuration,
+  OpenAIApi,
+  CreateModerationResponse,
+  CreateEmbeddingResponse,
+} from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { ApplicationError, UserError } from '@/lib/errors'
+
+const openAiKey = process.env.OPENAI_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+
+const config = new Configuration({
+  apiKey: openAiKey,
+})
+const openai = new OpenAIApi(config)
+
+export const runtime = 'edge'
+
+export default async function handler(req: NextRequest) {
+  try {
+    if (!openAiKey) {
+      throw new ApplicationError('Missing environment variable OPENAI_KEY')
+    }
+
+    if (!supabaseUrl) {
+      throw new ApplicationError('Missing environment variable SUPABASE_URL')
+    }
+
+    if (!supabaseServiceKey) {
+      throw new ApplicationError('Missing environment variable SUPABASE_SERVICE_ROLE_KEY')
+    }
+
+    const requestData = await req.json()
+
+    if (!requestData) {
+      throw new UserError('Missing request data')
+    }
+
+    const { prompt: query } = requestData
+
+    if (!query) {
+      throw new UserError('Missing query in request data')
+    }
+
+    const supabaseClient = createClient(supabaseUrl, supabaseServiceKey)
+
+    // Moderate the content to comply with OpenAI T&C
+    const sanitizedQuery = query.trim()
+    const moderationResponse: CreateModerationResponse = await openai
+      .createModeration({ input: sanitizedQuery })
+      .then((res) => res.json())
+
+    const [results] = moderationResponse.results
+
+    if (results.flagged) {
+      throw new UserError('Flagged content', {
+        flagged: true,
+        categories: results.categories,
+      })
+    }
+
+    // Create embedding from query
+    const embeddingResponse = await openai.createEmbedding({
+      model: 'text-embedding-ada-002',
+      input: sanitizedQuery.replaceAll('\n', ' '),
+    })
+
+    if (embeddingResponse.status !== 200) {
+      throw new ApplicationError('Failed to create embedding for question', embeddingResponse)
+    }
+
+    const {
+      data: [{ embedding }],
+    }: CreateEmbeddingResponse = await embeddingResponse.json()
+
+    const { error: matchError, data: pageSections } = await supabaseClient.rpc(
+      'match_page_sections',
+      {
+        embedding,
+        match_threshold: 0.78,
+        match_count: 10,
+        min_content_length: 50,
+      }
+    )
+
+    if (matchError) {
+      throw new ApplicationError('Failed to match page sections', matchError)
+    }
+
+    const tokenizer = new GPT3Tokenizer({ type: 'gpt3' })
+    let tokenCount = 0
+    let contextText = ''
+
+    for (let i = 0; i < pageSections.length; i++) {
+      const pageSection = pageSections[i]
+      const content = pageSection.content
+      const encoded = tokenizer.encode(content)
+      tokenCount += encoded.text.length
+
+      if (tokenCount >= 1500) {
+        break
+      }
+
+      contextText += `${content.trim()}\n---\n`
+    }
+
+    const prompt = codeBlock`
+      ${oneLine`
+        Your name is Jamie Neo.
+        You are a very enthusiastic Government Officer working for EMA in 
+        Singapore, who loves to help people! Use the the following sections from the 
+        EMA website to answer questions given by the user. The answer should be
+        outputted in markdown format. If you are unsure or the answer
+        is not explicitly written in the Context section you can infer the answer,
+        but caveat the answer by mentioning this is not mentioned on the EMA Website.
+      `}
+
+      Context sections:
+      ${contextText}
+
+      
+
+      Answer as markdown (embed links if it is mentioned in the Context sections) :
+    `
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages:[ {"role": "system", "content": prompt},{"role": "user", "content": sanitizedQuery}] ,
+      stream: true,
+    })
+
+    if (!response.ok) {
+      const error = await response.json()
+      throw new ApplicationError('Failed to generate completion', error)
+    }
+
+    // Transform the response into a readable stream
+    const stream = OpenAIStream(response)
+
+    // Return a StreamingTextResponse, which can be consumed by the client
+    return new StreamingTextResponse(stream)
+  } catch (err: unknown) {
+    if (err instanceof UserError) {
+      return new Response(
+        JSON.stringify({
+          error: err.message,
+          data: err.data,
+        }),
+        {
+          status: 400,
+          headers: { 'Content-Type': 'application/json' },
+        }
+      )
+    } else if (err instanceof ApplicationError) {
+      // Print out application errors with their additional data
+      console.error(`${err.message}: ${JSON.stringify(err.data)}`)
+    } else {
+      // Print out unexpected errors as is to help with debugging
+      console.error(err)
+    }
+
+    // TODO: include more response info in debug environments
+    return new Response(
+      JSON.stringify({
+        error: 'There was an error processing your request',
+      }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+  }
+}
