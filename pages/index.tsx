a7ef382dd9fdda6f40c45a422bc2c730653e40ce
Sean Koh <sean.koh.2015@smu.edu.sg>
2023-09-29 14:52:07 +0800

    Toss your dirty shoes into my washing machine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {UploadPage} from '@/components/UploadPage'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { SearchDialog } from '@/components/SearchDialog'
