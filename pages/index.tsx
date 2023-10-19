ec9350f8eb687eb11704a9e7783da6b320d59032
Sean Koh <sean.koh.2015@smu.edu.sg>
2023-10-19 16:16:26 +0800

    This is causing the problem?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Heead from 'next/head'
+import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { SearchDialog } from '@/components/SearchDialog'
 import Image from 'next/image'
@@ -7,29 +7,33 @@ import { HoverCard } from '@radix-ui/react-hover-card'
 
 export default function Home() {
   return (
-  <>
-    <Heead>
-      <title>DC Guidelines Search</title>
-      <meta
-        name="description"
-        content="This RAG Search runs on Next.js, Supabase, GPT-3, and a lotta coffee ☕."
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Heead>
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <SearchDialog/>
-      </div>
+    <>
+      <Head>
+        <title>DC Guidelines Search</title>
+        <meta
+          name="description"
+          content="This RAG Search runs on Next.js, Supabase, GPT-3, and a lotta coffee ☕."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <SearchDialog />
+        </div>
 
-      <div className="py-8 w-full flex items-center justify-center space-x-6">
-      <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-          <Link href="https://www.ura.gov.sg/Corporate/" className="flex items-center justify-center">
-          <p className="text-base mr-2">©2023 Urban Redevelopment Authority, Design and Planning Lab</p>
-          {/* <Image src={'/logo.webp'} width="60" height="26" alt="URA logo" /> */}
-          </Link>
-      </div>
-      <HoverCard>
+        <div className="py-8 w-full flex items-center justify-center space-x-6">
+          <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+            <Link
+              href="https://www.ura.gov.sg/Corporate/"
+              className="flex items-center justify-center"
+            >
+              <p className="text-base mr-2">
+                ©2023 Urban Redevelopment Authority, Design and Planning Lab
+              </p>
+            </Link>
+          </div>
+          {/* <HoverCard>
             <a
               href="https://www.ura.gov.sg/Corporate/Guidelines/Development-Control/"
               target="_blank"
@@ -42,10 +46,9 @@ export default function Home() {
                 alt="URA logo"
               />
             </a>
-          </HoverCard>
-
-      </div>
-  </main>
-  </>
+          </HoverCard> */}
+        </div>
+      </main>
+    </>
   )
 }
